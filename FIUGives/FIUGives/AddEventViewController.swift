dc00bbdf67e29f248bed21d5046ab996c4e10012
egumnova <egumn001@fiu.edu>
2017-06-15 01:04:48 -0400

    Updates to AddEvent view.

--- FIUGives/FIUGives/AddEventViewController.swift
+++ FIUGives/FIUGives/AddEventViewController.swift
@@ -8,23 +8,75 @@
 
 import UIKit
 
-class AddEventViewController: UIViewController {
-
-    @IBOutlet weak var addEvent: UIBarButtonItem!
+class AddEventViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
+    //MARK: properties
+    
+    @IBOutlet weak var eventNameTextField: UITextField!
+    @IBOutlet weak var eventFlyerURLTextField: UITextField!
+    @IBOutlet weak var eventContactNameTextField: UITextField!
+    @IBOutlet weak var eventContactEmailTextField: UITextField!
+    @IBOutlet weak var eventDescriptionTextView: UITextView!
+    @IBOutlet weak var eventLocationTextField: UITextField!
+    
+    //var eventCategory: String
+    var eventName: String?
+    var eventFlyerURL: String?
+    var eventContactName: String?
+    var eventDescription: String?
+    var eventLocationName: String?
+    
+    
+    @IBAction func addEvent(_ sender: UIBarButtonItem) {
+        
+        guard ((!(eventNameTextField.text?.isEmpty)!) && (!(eventContactNameTextField.text?.isEmpty)!) && (!(eventContactEmailTextField.text?.isEmpty)!) && (!(eventLocationTextField.text?.isEmpty)!)) else {
+            //present alert
+            let alertController = UIAlertController(title: "Event Not Created", message: "All fields must be completed", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .destructive)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true)
+            
+            return
+        }
+        
+        eventName = eventNameTextField.text!
+        eventFlyerURL = eventFlyerURLTextField.text!
+        
+    }
+
+    
+
+
     
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //handle text fields' user input through delegate callbacks
+        eventNameTextField.delegate = self
+        eventFlyerURLTextField.delegate = self
+        eventContactNameTextField.delegate = self
+        eventContactEmailTextField.delegate = self
+        eventDescriptionTextView.delegate = self
+        eventLocationTextField.delegate = self
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+
     
 
     /*
