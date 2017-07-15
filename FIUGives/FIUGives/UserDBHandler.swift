bcf66f2d9aeb9d5f2748798a1b28c6818dc0f1c1
Kathryn Bello <kbell005@fiu.edu>
2017-07-14 22:12:14 -0400

    user database

---
+++ FIUGives/FIUGives/UserDBHandler.swift
@@ -0,0 +1,13 @@
+//
+//  UserDBHandler.swift
+//  FIUGives
+//
+//  Created by Kathryn Bello on 7/13/17.
+//  Copyright © 2017 FIUGives. All rights reserved.
+//
+
+import UIKit
+
+class UserDBHandler: FIRAuth {
+
+}
